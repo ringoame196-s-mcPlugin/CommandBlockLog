687d1be0c2dd2f8122d74a23126b88cb1286a08b
りんご飴196 <kazumac41@gmail.com>
2024-10-11 21:06:22 +0900

    feat:CommandConst追加

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/CommandConst.kt
@@ -0,0 +1,15 @@
+package com.github.ringoame196_s_mcPlugin.commands
+
+object CommandConst {
+    // サブコマンド
+    const val START_LOG_COMMAND = "startlog"
+    const val STOP_LOG_COMMAND = "stoplog"
+    const val SHOW_LOG_COMMAND = "showlog"
+    const val SEE_INFO_COMMAND = "info"
+
+    // 検索値
+    const val SEARCH_IMPULSE = "b:impulse"
+    const val SEARCH_CHAIN = "b:chain"
+    const val SEARCH_REPEAT = "b:repeat"
+    const val SEARCH_PREFIX = "c:"
+}
