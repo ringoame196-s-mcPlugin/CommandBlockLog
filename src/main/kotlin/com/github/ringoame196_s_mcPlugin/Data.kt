222a4b9474ece2baa85e33e9d7909e07592fabe0
りんご飴196 <kazumac41@gmail.com>
2024-10-11 20:57:13 +0900

    feat:Dataクラス追加 ログ取得中かログlistを追加

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
@@ -0,0 +1,8 @@
+package com.github.ringoame196_s_mcPlugin
+
+import org.bukkit.block.Block
+
+object Data {
+    var inLogAcquisition = false
+    val commandBlockLog = mutableMapOf<Block,String>()
+}
