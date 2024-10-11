e16b2565f9c16c9252f1b230c35b9af0324a16b5
りんご飴196 <kazumac41@gmail.com>
2024-10-11 21:11:35 +0900

    feat:tabcompleterの実装

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/TabCompleter.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/TabCompleter.kt
@@ -6,6 +6,15 @@ import org.bukkit.command.TabCompleter
 
 class TabCompleter : TabCompleter {
     override fun onTabComplete(commandSender: CommandSender, command: Command, label: String, args: Array<out String>): MutableList<String>? {
-        return mutableListOf()
+        return when(args.size) {
+            1 -> mutableListOf(CommandConst.START_LOG_COMMAND, CommandConst.STOP_LOG_COMMAND, CommandConst.SHOW_LOG_COMMAND, CommandConst.SEE_INFO_COMMAND_BLOCK)
+            else -> {
+                when(args[0]) {
+                    // ログの情報を見るときのみ 検索値で指定ができるようにする
+                    CommandConst.SHOW_LOG_COMMAND -> mutableListOf(CommandConst.SEARCH_IMPULSE, CommandConst.SEARCH_CHAIN, CommandConst.SEARCH_REPEAT, CommandConst.SEARCH_COMMAND_PREFIX)
+                    else -> mutableListOf()
+                }
+            }
+        }
     }
 }
